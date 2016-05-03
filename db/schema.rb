a69eddaa3f2ec8d313ef2346c104c5c95c7e8da2
Alysia Brown <alysiacbrown@gmail.com>
2016-05-03 15:12:21 -0700

    Set up account registry and authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503213913) do
+ActiveRecord::Schema.define(version: 20160503215236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,8 +66,9 @@ ActiveRecord::Schema.define(version: 20160503213913) do
     t.date     "exp_date"
     t.integer  "cvv"
     t.integer  "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
