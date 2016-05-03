cf90f56e23722670d0118a7099c195ab65de3cea
Alysia Brown <alysiacbrown@gmail.com>
2016-05-03 10:22:35 -0700

    Update migration.

--- db/migrate/20160503164524_update_category_data_type.rb
+++ db/migrate/20160503164524_update_category_data_type.rb
@@ -1,6 +1,6 @@
 class UpdateCategoryDataType < ActiveRecord::Migration
   def change
-    change_column :products, :category, :array
+    change_column :products, :category, :text, array:true, default:[]
     add_column :products, :animal, :string
   end
 end
