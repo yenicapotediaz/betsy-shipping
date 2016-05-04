cf5ab7ec1511b8233c434d7ee8ee376c0ed46f57
Jessica <jessica.weeber@gmail.com>
2016-05-04 15:37:34 -0700

    working on checkboxes for filtering but not quite working yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
 	def index
 		@products = Product.all
 		@product_picker = Product.where.not(id: nil).sample(3)
-		@categories = Product.uniq.pluck(:category)
+		# @categories = Product.uniq.pluck(:category)
+		@categories = Product.where(:conditions => {:category => params[:category]})
 	end
+
 end
