e1818fda734037e65998a653a4d0257cfb5533c9
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-04 14:28:13 -0700

    migration added to change column name in orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503215236) do
+ActiveRecord::Schema.define(version: 20160504211945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20160503215236) do
   create_table "orders", force: :cascade do |t|
     t.string   "status"
     t.float    "price"
-    t.integer  "order_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
