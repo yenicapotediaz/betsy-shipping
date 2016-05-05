486bb7aac33f2de88294370621f7ceea6be71393
Jessica <jessica.weeber@gmail.com>
2016-05-05 09:36:30 -0700

    made some changes but pulling to get Deirdre's changes first and merging

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :users, :only => [:new, :create]
+  resources :users, :only => [:index, :new, :create]
   resources :products
 
   get '/products/category/:category' => 'products#show', as: 'product_category'
