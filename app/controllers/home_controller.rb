c2ae75bcf3429922c76dc6a625efd45c85a7b614
Jessica <jessica.weeber@gmail.com>
2016-05-04 18:35:52 -0700

    reverting back to what we had before with categories and will revist later once we get bootstrap.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
 	def index
 		@products = Product.all
 		@product_picker = Product.where.not(id: nil).sample(3)
-		# @categories = Product.uniq.pluck(:category)
-		@categories = Product.where(:conditions => {:category => params[:category]})
+		@categories = Product.uniq.pluck(:category)
 	end
 
 end
