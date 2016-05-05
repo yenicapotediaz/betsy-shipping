753258454989124abfbf31cbc768346e951f24ed
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-04 22:29:03 -0700

    think cart is working on a very basic level

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -3,7 +3,7 @@ class OrderitemsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @orderitem = current_order.orderitems.create(product: @product, quantity: 1 )
-    redirect_to root_path
+    redirect_to order_path(current_order.id)
   end
 
 end
