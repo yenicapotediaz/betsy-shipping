7563c8384f36dd0f9080242f9dea3213ee7d6f2b
Alysia Brown <alysiacbrown@gmail.com>
2016-05-05 12:49:06 -0700

    Added order and review seeds.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
 	belongs_to :product
+	belongs_to :user
 	validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
