6a380a4ce3455c9ea9b85ec230d9b1e328558112
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-04 22:59:05 -0700

    fixed pathing on product show page to adjust for the route i made

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,5 +1,9 @@
 class OrderitemsController < ApplicationController
 
+  def index
+    redirect_to order_path(current_order.id)
+  end
+
   def create
     @product = Product.find(params[:product_id])
     @orderitem = current_order.orderitems.create(product: @product, quantity: 1 )
