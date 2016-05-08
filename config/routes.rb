c5bad61b09bada21983bbb7ef483d8a67d410001
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-08 15:15:39 -0700

    wrote some tests for user

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   resources :sessions, :only => [:new, :create]
   get "/logout" => "sessions#destroy"
   get "/orders/:id/checkout" => 'orders#checkout', as: 'order_checkout'
-  patch "/orders/:id/checkout" => "orders#confirmation" as: 'order_confirmation'
+  patch "/orders/:id/checkout" => "orders#confirmation"
   get '/orders/:id/confirmation' => 'orders#confirmation', as: "order_confirmation"
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "validations: username can't be blank" do
+    user = User.new
+
+    assert_not user.valid?
+    assert user.errors.keys.include?(:username), "username is not in the errors hash"
+  end
+
+
+  test "validations: email can't be blank" do
+    user = User.new
+
+    assert_not user.valid?
+    assert user.errors.keys.include?(:email), "email is not in the errors hash"
+  end
+
 end
