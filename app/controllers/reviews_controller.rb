47063e508880d7faf92b10dc1ff111dea5d6eee9
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-10 10:25:28 -0700

    every time a review gets created it updated the products rating attribute

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,16 +8,32 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_create_params[:review])
 		if @review.save
+      update_product_rating(Product.find(@review.product_id))
     	redirect_to product_path(@review.product_id)
   	else
   		render :new
   	end
   end
 
+
   private
+
+  def update_product_rating(product)
+    ratings = product.reviews.collect { |a| a.rating.to_f }
+		overall = ratings.reduce(:+)/ratings.length
+		product.update(rating: overall)
+  end
 
   def review_create_params
 		params.permit(review: [:rating, :description, :product_id])
 	end
 
 end
+
+
+	# def show
+	# 	@product = Product.find(params[:id])
+	# 	@quantity = @product.quantity
+	# 	@quantity_numbers = (1..@quantity).to_a
+	# 	@rating = overall_rating(@product)
+	# end
