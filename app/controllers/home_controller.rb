63ea79771058b886fbba641d90f4e0bc5976be1e
Jessica <jessica.weeber@gmail.com>
2016-05-10 11:29:31 -0700

    controller methods to show only products that are not retired

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 	def index
-		@products = Product.all
-		@product_picker = Product.where.not(id: nil).sample(3)
+		@products = Product.where(retired: false)
+		@product_picker = Product.where(retired: false).where.not(id: nil).sample(3)
 		@animals = Product.uniq.pluck(:animal)
 		@categories = Product.uniq.pluck(:category)
 		@users = User.all		
