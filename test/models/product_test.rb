b83de0e5f593e5b378b4d5e2e4bb74b1f250539c
Jessica <jessica.weeber@gmail.com>
2016-05-10 16:45:25 -0700

    added some tests for products model and users model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,19 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   test "user can have many products" do
-    assert_includes users(:dog_merchant).products, products(:sushi_cat)
+    assert_includes users(:dog_merchant).products, products(:dog_pirate)
   end
+
+  test "display name for Dog Pirate should be Dog Pirate" do
+  	assert_equal "Dog Pirate", products(:dog_pirate).name
+  end
+
+  test "display name for Sushi Cat must be unique" do
+  	assert_equal "Sushi Cat", products(:sushi_cat).name
+  end
+
+  test "Price for Sushi Cat should be 200" do
+  	assert_equal 200, products(:sushi_cat).price
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,12 +9,37 @@ class UserTest < ActiveSupport::TestCase
     assert user.errors.keys.include?(:username), "username is not in the errors hash"
   end
 
-
   test "validations: email can't be blank" do
     user = User.new
 
     assert_not user.valid?
     assert user.errors.keys.include?(:email), "email is not in the errors hash"
+  end
+
+  test "can authenticate known users" do
+    doggy = users(:dog_merchant)
+    assert_equal doggy, User.log_in(doggy.username, "password")
+  end
+
+  test "won't authenticate mismatched data" do
+    assert_not User.log_in("my_username", "notpassword")
+  end
+
+  test "won't authenticate a known user with a bad password" do
+    kitty = users(:cat_merchant)
+    assert_not User.log_in(kitty.username, "guineapig")
+ end
+
+  test "Username for dog merchant should be dogs_yes" do
+    assert_equal "dogs_yes", users(:dog_merchant).username
+  end
+
+  test "User dog_merchant has the product dog_pirate" do
+    assert_includes users(:dog_merchant).products, products(:dog_pirate)
+  end
+
+  test "User cat_merchant has an order" do
+    assert_includes users(:cat_merchant).orders, orders(:second_order)
   end
 
 end
