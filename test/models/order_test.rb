97c6f66e554c20e699e610bffb79f656cbd0e7ea
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-10 16:45:17 -0700

    added some tests for order model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "order can has orderitems" do
+    assert_includes orders(:first_order).orderitems, orderitems(:first_orderitem)
+  end
+
+  test "order belongs to user" do
+    assert_includes users(:dog_merchant).orders, orders(:first_order)
+  end
+
 end
