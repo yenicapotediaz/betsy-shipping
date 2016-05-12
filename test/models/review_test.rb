1f2462f4631ff47e5f97cef6ad20db1c9a809ff2
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-11 17:54:27 -0700

    wrote test for review model

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "validations: rating can't be blank" do
+    review = Review.new
+
+    assert_not review.valid?
+    assert review.errors.keys.include?(:rating), "rating is not in the errors hash"
+  end
+
+  test "rating must be an integer" do
+    review = Review.new(rating: 3.4)
+    assert_not review.valid?
+    assert review.errors.keys.include?(:rating), "rating is not in the errors hash"
+  end
+
+  test "rating is valid if it is an integer" do
+    review = Review.new(rating: 3)
+    assert review.valid?
+  end
+
+  test "rating must be between 1 and 5" do
+    review = Review.new(rating: 7)
+    assert_not review.valid?
+    assert review.errors.keys.include?(:rating), "rating is not in the errors hash"
+  end
+
+
+
 end
