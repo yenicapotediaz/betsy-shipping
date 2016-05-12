7a2a16c2d9c738df70dbd73ae760486e94a02552
Jessica <jessica.weeber@gmail.com>
2016-05-12 13:13:24 -0700

    added some checks in the account page for when authenticated user doesn't have orders or products yet, as well as to know show a link to view products by a merchant if they don't have products.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
 		@product_picker = Product.where(retired: false).where.not(id: nil).sample(3)
 		@animals = Product.uniq.pluck(:animal)
 		@categories = Product.uniq.pluck(:category)
-		@users = User.all		
+		@users = User.all	
 	end
 end
