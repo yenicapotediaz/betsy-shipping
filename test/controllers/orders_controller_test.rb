f790fc745ae2b72f2beb04661ca1308d6ef85cad
Charles Ellis <hamled@hamled.name>
2016-11-07 09:03:59 -0800

    Controller tests for orders#shipping_set failure case

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -87,5 +87,28 @@ class OrdersControllerTest < ActionController::TestCase
         assert_equal @selected_method.cost, @order.shipping_cost
       end
     end
+
+    class Failure < ShippingSetTest
+      setup do
+        alias :send_request :send_base_request
+
+        def test_bad_request(id)
+          send_request shipping_method_id: id
+
+          assert_response :redirect
+          assert_redirected_to shipping_order_path
+          assert_not flash[:notice].empty?
+        end
+      end
+
+      test "should redirect to shipping selection page when bad id selected" do
+        bad_id = @shipping_methods.map { |s| s.id }.max * 2
+        test_bad_request(bad_id)
+      end
+
+      test "should redirect to shipping selection page when nil/no id selected" do
+        test_bad_request(nil)
+      end
+    end
   end
 end
