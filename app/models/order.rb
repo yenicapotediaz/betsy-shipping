1c6245ca2f593232e3bc5165466b71affcd6eb22
Charles Ellis <hamled@hamled.name>
2016-11-07 09:06:00 -0800

    Fix another issue uncovered by shipping_set tests
    This code for dealing with the shipping method for an order being set
    with nil was not done as valid Ruby, and I was using the wrong exception
    type anyways.

--- app/models/order.rb
+++ app/models/order.rb
@@ -37,7 +37,7 @@ class Order < ActiveRecord::Base
   # When we set the shipping method, we actually store
   # the name of the service and the cost
   def shipping_method=(method)
-    raise new ActiveRecord::AttributeAssignmentError unless method.present?
+    raise ArgumentError.new("method cannot be nil") unless method.present?
     self.shipping_name = method.name
     self.shipping_cost = method.cost
   end
